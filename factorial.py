0d24f7787baa5173a31d0a3902b3e73935d78f83
guyf <guy@bla.com>
2018-06-03 17:32:15 +0300

    update factorial file and added iterative func

--- factorial.py
+++ factorial.py
@@ -3,5 +3,13 @@ def factorial(n):
         return 1
     return n * factorial(n-1)
 
-#num = int(input("Enter number\n"))
-#print(factorial(num))
+def factorial2(n):
+    sum = 1
+    for i in range(1,n+1):
+        sum *= i
+    return sum
+
+
+num = int(input("Enter number\n"))
+print(factorial(num))
+print(factorial2(num))
