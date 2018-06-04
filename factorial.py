910aa4acedb193836c00e86daa8fdd17b3feb2c6
guyf <guy@bla.com>
2018-06-04 09:33:09 +0300

    after merge

--- factorial.py
+++ factorial.py
@@ -1,15 +1,36 @@
-def factorial(n):
+<<<<<<< HEAD
+=======
+# running time of factorial recursive ~1.1e-06
+# running time of factorial iterative ~8e-07
+import timeit
+
+def factorial_recursive(n):
+>>>>>>> 0a3d4f7... little decorations on factorial
     if n==0:
         return 1
-    return n * factorial(n-1)
+    return n * factorial_recursive(n-1)
 
-def factorial2(n):
+def factorial_iterative(n):
     sum = 1
     for i in range(1,n+1):
         sum *= i
     return sum
 
+<<<<<<< HEAD
 
-num = int(input("Enter number\n"))
-print(factorial(num))
-print(factorial2(num))
+=======
+t = timeit.Timer(lambda: factorial_recursive(10))
+t2 = timeit.Timer(lambda: factorial_iterative(10))
+sum1 = 0
+sum2 = 0
+num_times = 1000
+for i in range(num_times):
+    curr_time1 = t.timeit(number=1)
+    sum1 += curr_time1
+    curr_time2 = t2.timeit(number=1)
+    sum2 += curr_time2
+avg = sum1 / num_times
+avg2 = sum2 / num_times
+print('recursive time is: %.4E' % avg)
+print('iterative time is: %.4E' % avg2)
+>>>>>>> 0a3d4f7... little decorations on factorial
